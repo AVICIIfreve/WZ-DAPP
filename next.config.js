a31e50a3fc3387648d3b0aff755d584f64ceadf9
wuzhenyu <759799349@qq.com>
2025-02-19 15:24:39 +0800

    quick start NFT

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: [
+    "@ant-design",
+    "antd",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-input",
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
